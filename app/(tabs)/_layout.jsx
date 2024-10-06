f64f30c5167449f7902785a9f00e7b58099d726c
Madhini Maheshika <127973368+mdh00@users.noreply.github.com>
2024-10-06 10:19:07 +0530

    chore: initial setup

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,97 @@
+import { View, Text, Image } from 'react-native'
+import { Tabs, Redirect } from 'expo-router'
+
+import { icons } from '../../constants';
+
+const TabIcon = ({ icon, color }) => {
+    return (
+        <View className="items-center justify-center gap-2">
+            <Image
+                source={icon}
+                resizedMode="contain"
+                tintColor={color}
+                className="w-8 h-8"
+            />
+        </View>
+    )
+}
+const TabsLayout = () => {
+    return (
+        <>
+            <Tabs
+                screenOptions={{
+                    tabBarActiveTintColor: "#FFA001",
+                    tabBarInactiveTintColor: "#A8A8A8",
+                    tabBarShowLabel: false,
+                    tabBarStyle: {
+                        backgroundColor: "#FFFFFF",
+                        borderTopColor: "transparent",
+                        height: 84,
+                        borderTopLeftRadius: 20,
+                        borderTopRightRadius: 20,
+                        shadowColor: "#000",
+                        shadowOffset: { width: 0, height: 10 },
+                        shadowOpacity: 0.2,
+                        shadowRadius: 15,
+                        elevation: 5,
+                    },
+                }}
+            >
+                <Tabs.Screen
+                    name="home"
+                    options={{
+                        title: 'Home',
+                        headershown: false,
+                        tabBarIcon: ({ color }) => (
+                            <TabIcon
+                                icon={icons.home}
+                                color={color} />
+                        ),
+                    }}
+                />
+
+                <Tabs.Screen
+                    name="calendar"
+                    options={{
+                        title: 'Calendar',
+                        headershown: false,
+                        tabBarIcon: ({ color }) => (
+                            <TabIcon
+                                icon={icons.calendar}
+                                color={color} />
+                        ),
+                    }}
+                />
+
+                <Tabs.Screen
+                    name="notification"
+                    options={{
+                        title: 'Notification',
+                        headershown: false,
+                        tabBarIcon: ({ color }) => (
+                            <TabIcon
+                                icon={icons.bell}
+                                color={color} />
+                        ),
+                    }}
+                />
+
+                <Tabs.Screen
+                    name="profile"
+                    options={{
+                        title: 'Profile',
+                        headershown: false,
+                        tabBarIcon: ({ color }) => (
+                            <TabIcon
+                                icon={icons.profile}
+                                color={color} />
+                        ),
+                    }}
+                />
+
+            </Tabs>
+        </>
+    )
+}
+
+export default TabsLayout
---
+++ app/index.jsx
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View, ScrollView } from 'react-native';
+import { Link } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function App() {
+  return (
+    <SafeAreaView className="bg-primary h-full">
+     <ScrollView
+        contentContainerStyle={{
+          height: "100%",
+        }}
+      >
+      <View className="flex-1 items-center justify-center bg-primary">
+            <Text className="text-3xl text-black font-bold text-center">
+              Wellness{"\n"}
+              Pioneers{" "}
+            </Text>
+            <Link href="/home" style={{color:'blue'}}>Home</Link> 
+          </View>  
+      </ScrollView>
+    </SafeAreaView>
+  );
+
+}
